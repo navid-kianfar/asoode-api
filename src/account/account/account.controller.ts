1cc9593aeb3c556876e10058364e1b21de240560
navidkianfar <t5bushVeJn2TDaFYmWZ7Xseb6XcQjqfkmFb5uFeUJF3wxUmJbgcLTXbVm3Zj83Lp>
2023-02-26 00:53:25 +0300

    asoode-common package

--- src/account/account/account.controller.ts
+++ src/account/account/account.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SigninRequest, SignupRequest } from 'asoode-common';
+import { AccountService } from './account.service';
 
 @Controller('account')
-export class AccountController {}
+export class AccountController {
+  constructor(private readonly accountService: AccountService) {}
+
+  @Post('signin') async signin(@Body() model: SigninRequest) {
+    return await this.accountService.signin(model);
+  }
+  @Post('signup') async signup(@Body() model: SignupRequest) {
+    return await this.accountService.signup(model);
+  }
+  // @Post('forgot') async forgot(@Body() request: ForgotRequest) {}
+}
--- src/account/account/account.service.ts
+++ src/account/account/account.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import {
+  SigninRequest,
+  SigninResponse,
+  SignupRequest,
+  SignupResponse,
+  OperationResult,
+} from 'asoode-common';
 
 @Injectable()
-export class AccountService {}
+export class AccountService {
+  async signin(
+    model: SigninRequest,
+  ): Promise<OperationResult<SigninResponse>> {throw new Error();}
+  async signup(
+    model: SignupRequest,
+  ): Promise<OperationResult<SignupResponse>> { throw new Error(); }
+}
